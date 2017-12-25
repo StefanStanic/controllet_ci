9c74bed9d382d69e03ccfe217473efa54ecf9525
StefanStanic <vts.stefan.stanic@gmail.com>
2017-12-25 16:38:52 +0100

    fixed paying bills, added transaction history

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -20,14 +20,26 @@ class dashboard_model extends CI_Model
             return false;
         }
     }
-
         public function pay_my_bills($key){
             $data=array(
                 'active'=>2
             );
             $this->db->where('id_montly_bills',$key);
             $query=$this->db->update('recurring_montly_bills',$data);
-            if($query){
+
+
+            $this->db->where('id_montly_bills',$key);
+            $query1=$this->db->get('recurring_montly_bills');
+            $row=$query1->row();
+            $transactions=array(
+                "date_of_transaction"=>date("Y-m-d h:i:sa"),
+                "category"=>$row->category,
+                "transaction_amount"=>$row->amount,
+                "id_user"=>$row->id_user
+            );
+
+            $query2=$this->db->insert('transactions',$transactions);
+            if($query and $query2){
                 return true;
             }else {
                 return false;
